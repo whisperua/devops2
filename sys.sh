9e9f72f5e1de125bb5d4eeabd0e3b3fb872b84c3
Tony <attakai@gmail.com>
2024-05-14 21:38:21 +0000

    add script

---
+++ sys.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Show OS version
+echo "Current OS:"
+lsb_release -si
+
+echo -e "\nCurrent OS Version:"
+lsb_release -sr
+
+# List users with Bash shell
+echo -e "\nUsers with Bash shell:"
+grep '/bin/bash' /etc/passwd | cut -d ":" -f 1
+
+# Show open ports
+echo -e "\nOpen Ports:"
+ss -tuln | awk 'NR > 1 {print $1, $4}'
